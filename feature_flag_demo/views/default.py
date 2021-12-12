4517bc4e2c7fc8b9469d05483ca0708fe061c2b2
Dan Fields <daniel.laurent.fields@gmail.com>
2021-12-12 10:11:41 -0500

    updates

--- feature_flag_demo/views/default.py
+++ feature_flag_demo/views/default.py
@@ -1,6 +1,48 @@
 from pyramid.view import view_config
+from appconfig_helper import AppConfigHelper
+
+# #1234postgres1234
+# aws_app_config = AppConfigHelper(
+#     "ApiAppConfig",
+#     "production",
+#     "ff-flag",
+#     15  # minimum interval between update checks
+# )
+
+from UnleashClient import UnleashClient
+
+
+
+from configparser import ConfigParser
+config = ConfigParser()
+
+config.read('feature_flag_config.ini')
+
+unleash_client = UnleashClient(
+    url=config.get('unleashed', 'endpoint'),
+    app_name="feature_flag_demo",
+    environment=config.get('unleashed', 'environment'),
+    custom_headers={'Authorization': config.get('unleashed', 'api_secret_key')})
+
+
+unleash_client.initialize_client()
 
 
 @view_config(route_name='home', renderer='feature_flag_demo:templates/mytemplate.jinja2')
 def my_view(request):
-    return {'project': 'feature_flag_demo'}
+
+    # if aws_app_config.update_config():
+    #     print("new config received")
+
+    return {'project': 'feature_flag_demo',
+            # 'aws_app_config': aws_app_config.config
+            'flags': {
+                'unleashed': {
+                    "initialized": unleash_client.is_initialized,
+                    "demo_test": unleash_client.is_enabled("demo_test"),
+                    "demo_release": unleash_client.is_enabled("demo_release"),
+                    "wat": unleash_client.is_enabled("wat")
+                }
+            }
+
+    }
--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ requires = [
     'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'waitress',
+
+    'sample-helper-aws-appconfig',
+    'UnleashClient'
 ]
 
 tests_require = [
